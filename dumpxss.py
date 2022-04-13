41e462e8ce7d7dae43c9b2e7e8faf697d505bb21
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2022-04-13 19:54:38 +0300

    Update dumpxss.py
    4 changes made

--- dumpxss.py
+++ dumpxss.py
@@ -1,27 +1,32 @@
+#!/usr/bin/env python
+
+import sys
 import requests
 import threading
 from colorama import Fore, Back, Style
+import time
 
 
 print(
         Fore.YELLOW +
          """
-________                                             
+________
 \______ \  __ __  _____ _________  ___  ______ ______
  |    |  \|  |  \/     \\____ \  \/  / /  ___//  ___/
- |    `   \  |  /  Y Y  \  |_> >    <  \___ \ \___ \ 
+ |    `   \  |  /  Y Y  \  |_> >    <  \___ \ \___ \
 /_______  /____/|__|_|  /   __/__/\_ \/____  >____  >
-        \/            \/|__|        \/     \/     \/ 
-
+        \/            \/|__|        \/     \/     \/
                                           [ twitter.com/r00t_nasser ]
                                           [ Snapchat : aaa.saq ]
         """ + Fore.RESET)
 
 print()
 print()
-file = open('url.txt','r')
+
+file = open(sys.argv[1],'r')
 payloads = open('payloads.txt','r')
 def Send_req(url,payload):
+    time.sleep(0.15)
     #while url[-1] != '=':
      #   url = url[:-1]
     url = url.replace("=",f"={payload}")
@@ -31,7 +36,10 @@ def Send_req(url,payload):
         res = requests.get(url)
         if payload in res.text:
            print(Fore.GREEN +'XSS Found   -->','   ' , f"{url}" + Fore.RESET)
-
+           print(Fore.GREEN , f"{url}" + Fore.RESET, file=open('output.txt','w'))
+           #print(Fore.GREEN +'XSS Found   -->','   ' , f"{url}" + Fore.RESET, file=open('output.txt','a'))
+        else:
+           print(Fore.RED +'XSS NOT Found   -->','   ' , f"{url}" + Fore.RESET)
 
     except Exception as e:
         pass
